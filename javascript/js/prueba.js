b0fbcdacefbba3b3a892dca33e82cbfb1a02e39f
DESKTOP-PUN0B2F\Mario Avila <marioavi@gmail.com>
2023-05-04 05:41:26 -0500

    js
    js CT

--- javascript/js/prueba.js
+++ javascript/js/prueba.js
@@ -1,11 +1,20 @@
 const formulario = document.getElementById("formulario");
-const numero1 = formulario.elements["numero1"].value;
-const numero2 = formulario.elements["numero2"].value;
+const numero1 = formulario.elements["numero1"];
+const numero2 = formulario.elements["numero2"];
 
-const sumar = () =>{
-    alert (numero1 + numero2 );
 
-    const  resultado = document.getElementById("salida");
-    resultado.innerHTML="numero1 + numero2" ;
+
+const sumar = () => {
+    const num1 = document.getElementById("tx1");
+    const num2 = document.getElementById('tx2').value;
+    const resultado = document.getElementById("salida");
+    //resultado.innerHTML = numero1 + numero2 ;
+    //alert (numero1 + numero2 );
+    let sumita =  parseFloat(numero1.value) + parseFloat(numero2.value);
+    resultado.innerText= sumita;
+    console.log("Resultado ==>" + num2);
+    document.getElementById('tx3').value= sumita ;
+
+    
 
 }
